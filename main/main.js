b77c6561e98dd36fb8139dd8d6808161510bdbcd
sowmiyamkp <sowmiyamkp1305@gmail.com>
2023-03-25 19:19:15 -0700

    add array-method

---
+++ main/main.js
@@ -0,0 +1,33 @@
+//push
+var a=[10,30,40];
+var y=a.push(45,50);
+console.log(y);
+console.log(a);       
+
+//pop
+var b=[20,56,26,78];
+var x=b.pop();
+console.log(b);
+
+//shift
+
+var c=[20,30,22];
+ c.shift();
+console.log(c);
+
+//unshift
+
+var d=[22,10,30];
+d.unshift(33);
+console.log(d);
+
+//
+var a=[1,2,3,4];
+let array=[6,8,9];
+a.push(array); 
+console.log(array);
+
+
+var b=[20,56,26,78];
+var x=b.pop(50);
+console.log(b);
